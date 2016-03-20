fb7fec4c09582777a46f433fd28a1090d36d8b48
pgadea7 <pgadea7@gmail.com>
2016-03-19 20:51:44 -0400

    single landing page design

---
+++ ajaxSubmit.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$name = $_POST['message'];
+
+$header = "From:" . $email . "\r\n";
+$header .= "X-Mailer: PHP /" . phpversion() . "\r\n";
+$header .= "Mime Version: 1.0 \r\n";
+$header .= "Content Type: text/plain";
+
+$comment = "This message has been sent by " . $name . "\r\n";
+$comment .= "E-mail it's: ". $email . "\r\n";
+$comment .= 'His message is: '$message . "\r\n";
+
+$for = "pgadea7@hotmail.com";
+$subject = "Contact from website";
+
+mail($subject, utf8_decode($comment), $header );
+
+echo json_encode(array(
+      'Message' => sprintf("Your message has been sent %s", $name);
+));
